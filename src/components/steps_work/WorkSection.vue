21637c82e5e062899a77a06baf6214f978aa8d61
anasteyshha6 <glazunova.anastassia@yandex.ru>
2024-07-04 01:03:55 +0300

    go

--- src/components/steps_work/WorkSection.vue
+++ src/components/steps_work/WorkSection.vue
@@ -28,35 +28,35 @@ export default {
     return {
       steps: [
         {
-          imageSrc: "/public/imgs/water.svg",
+          imageSrc: "/imgs/water.svg",
           imageAlt: "water",
           stepTitle: "1 ЭТАП. ОЦЕНКА МЕСТНОСТИ",
           stepDescription:
             "Наша команда специалистов проводит тщательную оценку местности, чтобы определить наилучшее место для бурения скважины, учитывая геологические особенности и потребности клиента.",
         },
         {
-          imageSrc: "/public/imgs/water.svg",
+          imageSrc: "/imgs/water.svg",
           imageAlt: "water",
           stepTitle: "2 ЭТАП. ПОДГОТОВКА ОБОРУДОВАНИЯ",
           stepDescription:
             "Мы используем современное оборудование и технологии для проведения бурения скважин, что позволяет нам обеспечить высокую эффективность и точность работ.",
         },
         {
-          imageSrc: "/public/imgs/water.svg",
+          imageSrc: "/imgs/water.svg",
           imageAlt: "water",
           stepTitle: "3 ЭТАП. БУРЕНИЕ СКВАЖИН",
           stepDescription:
             "Наша бригада осуществляет бурение скважин с соблюдением всех технических и безопасностных стандартов, чтобы обеспечить надежное водоснабжение для клиентов.",
         },
         {
-          imageSrc: "/public/imgs/water.svg",
+          imageSrc: "/imgs/water.svg",
           imageAlt: "water",
           stepTitle: "4 ЭТАП. ТЕХНИЧЕСКОЕ ОБСЛУЖИВАНИЕ",
           stepDescription:
             "После завершения работ, мы предоставляем клиентам техническое обслуживание и консультации по эксплуатации скважины, чтобы обеспечить долгосрочную надежность системы водоснабжения.",
         },
         {
-          imageSrc: "/public/imgs/water.svg",
+          imageSrc: "/imgs/water.svg",
           imageAlt: "water",
           stepTitle: "5 ЭТАП. ГАРАНТИЯ КАЧЕСТВА",
           stepDescription:
